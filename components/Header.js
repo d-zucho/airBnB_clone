af52aeac7dff7096504d5a8b3d6f6419353dbd9c
D_Zucho <danny.nico.dev@outlook.com>
2021-10-13 16:41:26 -0700

    tailwind header fix 1

--- components/Header.js
+++ components/Header.js
@@ -21,18 +21,19 @@ function Header() {
       </div>
 
       {/* middle */}
-      <div className="md:shadow-sm overflow-hidden flex items-center re border-2 py-2 rounded-full">
+
+      <div className="relative flex w-full items-center">
         <input
           type="text"
-          placeholder="Search Location"
-          className="pl-5 bg-transparent outline-none flex-grow placeholder-gray-300 text-gray-600"
+          placeholder="Placeholder"
+          className="py-2 px-2 md:px-3 md:py-3 text-sm placeholder-blueGray-300 text-gray-500 relative bg-white border border-blueGray-300 outline-none focus:outline-none focus:ring w-full pr-10 rounded-full md:text-base "
         />
-        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full mr-2 object-contain flex-shrink-0 p-2 cursor-pointer md:mx-2" />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full absolute right-1 mr-2 object-contain flex-shrink-0 p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* right */}
       <div className="flex space-x-4 items-center justify-end text-gray-500">
-        <p className="hidden md:inline pl-2">Become a host</p>
+        <p className="hidden md:inline pl-4">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
 
         <div className="flex border-2 p-2 rounded-full space-x-2 cursor-pointer">
